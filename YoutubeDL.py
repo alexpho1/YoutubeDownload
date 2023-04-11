e0fffa782082d5a210dbc29186a6174cf03935bb
alexpho1 <115561538+alexpho1@users.noreply.github.com>
2023-04-11 15:55:26 -0400

    Add files via upload

---
+++ YoutubeDL.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+from sys import argv
+
+link = input("Paste the link to youtube video here: ")
+yt = YouTube(link)
+
+print('Title: ', yt.title)
+print('Views: ', yt.views)
+
+dl = yt.streams.get_highest_resolution()
+dl.download('/Users/alex.phommachanh/Downloads')
